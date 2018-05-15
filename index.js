464ce29dd36d498101396c5f2671cb57a93f9581
Matthew Retoff <matt.retoff@yahoo.com>
2018-05-15 16:14:55 -0400

    Display color in a div

--- index.js
+++ index.js
@@ -10,14 +10,24 @@ const handleSubmit = function(ev) {
     //Create separate items for list
     const nameItem = document.createElement('li')
     nameItem.textContent = `Name: ${user}`
+
     const ageItem = document.createElement('li')
     ageItem.textContent = `Age: ${age}`
+
+    const colorItem = document.createElement('li')
+    colorItem.textContent = 'Favorite Color:'
+    const div = document.createElement('div')
+    div.style.backgroundColor = color
+    div.style.width = '6rem'
+    div.style.height = '3rem'
+    colorItem.appendChild(div)
     
     //Add items to list and add list to the page
     const userList = document.querySelector('#users')
     const list = document.createElement('ul')
     list.appendChild(nameItem)
     list.appendChild(ageItem)
+    list.appendChild(colorItem)
     userList.appendChild(list)
 
     f.reset()
