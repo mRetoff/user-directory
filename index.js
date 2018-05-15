f8f51fd0bcc0ee45b7c7e0d527c7e1be18158ff4
Matthew Retoff <matt.retoff@yahoo.com>
2018-05-15 16:04:33 -0400

    Display in an unordered list

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const form = document.querySelector("#userForm")
 
 const handleSubmit = function(ev) {
     ev.preventDefault()
-    const userList = document.querySelector('#users')
     const f = ev.target
     const user = f.name.value
     const age = f.age.value
     const color = f.color.value
 
-    const p = document.createElement('p')
-    p.style.backgroundColor = color
-    p.textContent = `${user}, ${age}`
-    userList.appendChild(p)
+    //Create separate items for list
+    const nameItem = document.createElement('li')
+    nameItem.textContent = `Name: ${user}`
+    const ageItem = document.createElement('li')
+    ageItem.textContent = `Age: ${age}`
+    
+    //Add items to list and add list to the page
+    const userList = document.querySelector('#users')
+    const list = document.createElement('ul')
+    list.appendChild(nameItem)
+    list.appendChild(ageItem)
+    userList.appendChild(list)
 
     f.reset()
     f.name.focus()
