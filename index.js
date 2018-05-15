340775ea1a6453df2071396e403d57dea7caa479
Matthew Retoff <matt.retoff@yahoo.com>
2018-05-15 14:41:52 -0400

    Add each user to a list on screen on the same line

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const form = document.querySelector("#userForm")
 
 const handleSubmit = function(ev) {
-    const heading = document.querySelector('#Heading1')
+    const userList = document.querySelector('#users')
     ev.preventDefault()
     const f = ev.target
     const user = f.name.value
-    heading.textContent = user
+    userList.textContent += " " + user
 }
 form.addEventListener('submit', handleSubmit)
